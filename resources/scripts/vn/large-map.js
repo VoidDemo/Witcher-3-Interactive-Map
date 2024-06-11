e8489edc30eea6dfa5b095f25685597bd76ea79e
VoidDemo <115409422+VoidDemo@users.noreply.github.com>
2024-06-11 18:58:14 +0200

    Add files via upload

---
+++ resources/scripts/vn/large-map.js
@@ -0,0 +1,26 @@
+export function createLargeMapLayer(map) {
+    var bounds = [];
+    var startX = -0.12;
+    var startY = 51.53;
+    var width = 0.03;
+    var height = 0.02;
+    var cols = 8;
+    var rows = 9;
+
+    var layerGroup = L.layerGroup();
+    
+    for (var col = 0; col < cols; col++) {
+        for (var row = 0; row < rows; row++) {
+            var west = startX + col * width;
+            var east = west + width;
+            var north = startY - row * height;
+            var south = north - height;
+            bounds.push([[north, west], [south, east]]);
+            var imageUrl = `/resources/maps/hos_velen/large/${col}/${8 - row}.jpg`;
+            var index = col * rows + row;
+            L.imageOverlay(imageUrl, bounds[index]).addTo(layerGroup);
+        }
+    }
+    
+    return layerGroup;
+}
---
+++ resources/scripts/wo/small-map.js
@@ -0,0 +1,29 @@
+export function createSmallMapLayer(map) {
+    var bounds = [];
+    var startX = -0.12;
+    var startY = 51.53;
+    var width = 0.03;
+    var height = 0.02;
+    var cols = 3;
+    var rows = 2;
+
+    var layerGroup = L.layerGroup();
+    
+    for (var col = 0; col < cols; col++) {
+        for (var row = 0; row < rows; row++) {
+            var west = startX + col * width;
+            var east = west + width;
+            var north = startY - row * height;
+            var south = north - height;
+            bounds.push([[north, west], [south, east]]);
+            var fileExtension = col === 2 ? 'png' : 'jpg';
+            var imageUrl = `/resources/maps/white_orchard/small/${col}/${1 - row}.${fileExtension}`;
+            var index = col * 2 + row;
+            L.imageOverlay(imageUrl, bounds[index]).addTo(layerGroup);
+        }
+    }
+    
+    return layerGroup;
+}
+
+
