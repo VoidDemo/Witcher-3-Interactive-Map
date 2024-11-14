147bc6d7865114c89217c0524c3f43c2fb26ca3c
Błażej Klimas <115409422+VoidDemo@users.noreply.github.com>
2024-11-14 16:41:22 +0100

    Delete files directory

--- files/scripts/login-window.js
+++
@@ -1,9 +0,0 @@
-function openLoginWindow(page) {
-    var url = page === 'login' ? '../../pages/login.html' : '../../pages/register.html'; // Przykładowe strony
-    window.open(url, 'LoginWindow', 'width=800,height=600,left=200,top=200');
-}
-
-function closeWindowAndReload() {
-    window.opener.location.reload(); // Przeładowanie strony głównej
-    window.close(); // Zamknięcie obecnego okna
-}
